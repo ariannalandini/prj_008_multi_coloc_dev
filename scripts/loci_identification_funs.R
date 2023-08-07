b866fb6827fa2aac62123a23db36f906beb2c64c
Arianna Landini (ext) <arianna.landini@hnode02.cm.cluster>
2023-08-07 13:07:20 +0200

    Remove not used code from identification of overlapping loci step

--- scripts/loci_identification_funs.R
+++ scripts/loci_identification_funs.R
@@ -77,72 +77,22 @@ locus.breaker <- function(
 
 ### locus.lister
 locus.lister <- function(
-  my_path_gwas=NULL,
-  traits_pref="",
-  traits_suf=".regenie.gz",
-  p.sig = 5e-08,
-  p.limit = 1e-05,
-  hole.size = 250000,
   my_path_loci=NULL,
-  SNP="ID",
-  CHR="CHROM",
-  BP="GENPOS",
-  A1="ALLELE1",
-  A2="ALLELE0",
-  BETA="BETA",
-  SE="SE",
-  P="P",
   out="./panlocus_table.tsv"){
   
   require(data.table)
   require(GenomicRanges)
   require(dplyr)
-#  source("/group/pirastu/prj_004_variant2function/scripts/locusbreaker_AL.R")
-  
-  if(is.null(my_path_loci)){
-    gwas <- list.files(path=my_path_gwas, pattern="*")
-    cat(paste0("\n", length(gwas), " files found in ",my_path_gwas, " folder\n"))
-    SNPs <- list()
-    list_of_files <- list()
-    # load the sumstats and put them into a list
-    for(i in 1:length(gwas)){
-      list_of_files[[i]] <- fread(paste0(my_path_gwas, "/", gwas)[[i]], data.table = F)
-      cat(paste0("\n", gwas[[i]], " loaded\n"))
-      SNPs[[i]] <- list_of_files[[i]][[SNP]]
-    }
-    cat("\nAll GWAS summary statistics loaded!\n")
-    shared_SNPs <- Reduce(intersect, SNPs) # compute the SNPs that are present in all the summary stats
-    
-    if(is.null(gwas_traits)){gwas_traits <- gsub(paste0(traits_pref, "(.*)", traits_suf), "\\1", gwas)}
-    names(list_of_files) <- unlist(gwas_traits)
-    
-    loci <- list()
-    for(i in 1:length(gwas)){
-      to_take <- gwas_traits[[i]]
-      sstats <- list_of_files[[to_take]]
-      colnames(sstats) <- toupper(colnames(sstats))   
-      sstats <- sstats %>% select(one_of(c(SNP, CHR, BP, A2, A1, BETA, SE, P)))
-      sstats <- sstats[which(sstats[[SNP]] %in% shared_SNPs),] # select only the SNPs that are shared among all the traits     
-      loci[[i]] <- locus.breaker(sstats, p.label=P, chr.label=CHR, pos.label=BP, p.sig=p.sig, p.limit=p.limit, hole.size=hole.size)
-      loci[[i]]$trait <- rep(to_take, nrow(loci[[i]]))
-      loci[[i]]$path <- rep(paste0(my_path_gwas, "/", gwas[[i]]), nrow(loci[[i]]))
-      cat(paste0("\nTrait-specific loci identified for ", to_take, "\n"))
-    }
-    cat("\nTrait-specific loci identified for all traits!\n")
-  } else{
-# If you have precomputed trait-specific loci, generated with locus.breaker function
-    loci_list <- list.files(path=my_path_loci, pattern = "*_loci.tsv", full.names=T)
-    loci <- lapply(loci_list, function(x) fread(x, data.table=F))
-  }
 
-  all_loci <- do.call(rbind, loci) # create the list of all the loci
+  loci_list <- list.files(path=my_path_loci, pattern="*_loci.tsv", full.names=T)
+  all_loci <- as.data.frame(rbindlist(lapply(loci_list, function(x) fread(x, data.table=F))))
   cat("\nIdentify overlapping loci across traits\n")
   
   pan_loci <- reduce(GRanges(seqnames = all_loci$chr, IRanges(as.numeric(all_loci$start), as.numeric(all_loci$end)))) 
   pan_loci_non_reduced <- GRanges(seqnames = all_loci$chr, IRanges(as.numeric(all_loci$start), as.numeric(all_loci$end)))
   
-  overlapping <- findOverlaps(pan_loci_non_reduced, pan_loci) #find overlaps between all the loci and use it as an index of the unique non overlapping loci
-  all_loci$pan_locus <- rep(0, nrow(all_loci)) #allocate the column 
+  overlapping <- findOverlaps(pan_loci_non_reduced, pan_loci) # find overlaps between all the loci and use it as an index of the unique non overlapping loci
+  all_loci$pan_locus <- rep(0, nrow(all_loci)) # allocate the column 
   all_loci[overlapping@from,]$pan_locus <- overlapping@to  # assigning the number as index of which macro loci is overlapping 
   all_loci$pan_locus_name <- rep(0, nrow(all_loci))
   
@@ -152,6 +102,6 @@ locus.lister <- function(
   }
   rownames(all_loci) <- NULL
   cat(paste0("\nTotal of ", length(unique(all_loci$pan_locus)), " pan loci identified\n"))
-  fwrite(all_loci, out, sep="\t", quote=F, na=NA)
+  fwrite(all_loci %>% arrange(pan_locus, trait), out, sep="\t", quote=F, na=NA)
   #  return(all_loci)
 }
--- scripts/locus_lister_wrap.R
+++ scripts/locus_lister_wrap.R
@@ -1,80 +1,20 @@
 #### Pietro's script to identify overlapping loci across multiple traits and collapse them in a mega locus
 
-### Must provide: 
-# 1) my_path_gwas - path to GWAS
-# 2) traits_pref AND traits_sub - prefix and suffix of GWAS files
-
-## Other arguments should have default options - to revisit once established the tileDB format
-
-
-### TO DO:
-# Add if/else statement to check these conditions are met
-
-
 library(optparse)
 
 ### Function
 source("prj_008_multi_coloc_dev/scripts/loci_identification_funs.R")
 
 ### Set arguments
-
 option_list <- list(
-  make_option("--gwas_path", type="character", default=NULL, 
-              help="Path to directory having all GWAS summary statistics", metavar="character"),
-  make_option("--pref", type="character", default="", 
-              help="Instead of providing the list of traits names, you can provide the file name prefix to the trait (to be removed)", metavar="character"),  
-  make_option("--suf", type="character", default=".regenie.gz", 
-              help="Instead of providing the list of traits names, you can provide the file name suffix to the trait (to be removed)", metavar="character"),
-  make_option("--sig_pval", type="numeric", default=5e-08, 
-              help="Significant p-value threshold for top hits", metavar="numeric"),
-  make_option("--limit", type="numeric", default=1e-05, 
-              help="P-value threshold for loci borders", metavar="numeric"),  
-  make_option("--hole", type="integer", default=250000,
-              help="Minum pair-base distance between SNPs in different loci", metavar="integer"),
   make_option("--loci_path", type="character", default=NULL, 
               help="Path to directory having all precomputed loci", metavar="character"),
-  make_option("--snp", type="character", default="ID", 
-              help="Name of rsid column", metavar="character"),  
-  make_option("--chr", type="character", default="CHROM",
-              help="Name of chromosome column", metavar="integer"),
-  make_option("--pos", type="character", default="GENPOS", 
-              help="Name of genetic position column", metavar="character"),  
-  make_option("--a1", type="character", default="ALLELE1", 
-              help="Name of effect allele column", metavar="character"),  
-  make_option("--a0", type="character", default="ALLELE0", 
-              help="Name of NON effect allele column", metavar="character"),  
-  make_option("--effect", type="character", default="BETA", 
-              help="Name of effect size column", metavar="character"),  
-  make_option("--se", type="character", default="SE", 
-              help="Name of standard error of effect column", metavar="character"), 
-  make_option("--pval", type="character", default="P", 
-              help="Name of p-value of effect column", metavar="character"),
   make_option("--out", type="character", default="./panlocus_table.tsv", 
               help="Path and name of output directory and file", metavar="character")
 ); 
-
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-
 ### Run locus_lister!
-
-locus.lister(
-  my_path_gwas=opt$path,
-  traits_pref=opt$pref,
-  traits_suf=opt$suf,
-  p.sig=opt$sig_pval,
-  p.limit=opt$limit,
-  hole.size=opt$hole,
-  my_path_loci=opt$loci_path,
-  SNP=opt$snp,
-  CHR=opt$chr,
-  BP=opt$pos,
-  A1=opt$a1,
-  A2=opt$a0,
-  BETA=opt$effect,
-  SE=opt$se,
-  P=opt$pval,
-  out=opt$out)
+locus.lister(my_path_loci=opt$loci_path, out=opt$out)
 cat("\n**** DONE!! ****\n")
-
